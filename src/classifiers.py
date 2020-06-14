6d52c79bfab234afbbb8456cab089454e139d5fe
Dhruva Bansal <dbansal36@gatech.edu>
2020-06-14 23:11:25 +0530

    Complete classifier and dataUtil files

---
+++ src/classifiers.py
@@ -0,0 +1,137 @@
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, StackingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.svm import SVC
+import numpy as np
+from itertools import zip_longest
+import matplotlib.pyplot as plt
+import random
+from sklearn.metrics import auc
+import statistics
+from tqdm.notebook import tqdm
+
+random.seed(42)
+
+class CustomEnsembleClassifier:
+    def __init__(self, clfs):
+        self.classifiers = clfs
+    
+    def predict(self, X):
+        probabilities = None
+        for clf in self.classifiers:
+            if probabilities is None:
+                probabilities = clf.predict_proba(X)
+            else:
+                probabilities += clf.predict_proba(X)
+        return np.argmax(np.array(probabilities), axis=1)
+
+def trainLogisticRegressionClassifier(X, Y, verbose=True):
+    if verbose:
+        print("Traning Logistic Regression Classifier")
+    clf = LogisticRegression(random_state=42)
+    clf.fit(X, Y)
+    return clf
+
+def trainMLPClassifier(X, Y, verbose=True):
+    if verbose:
+        print("Training MLP Classifier")
+    clf = MLPClassifier(hidden_layer_sizes=(256, 1024, 2048, 1024, 256), random_state=42,
+                        max_iter=200, learning_rate='adaptive', learning_rate_init=0.0001, activation='relu',
+                        verbose=True)
+    clf.fit(X, Y)
+    return clf
+
+def trainKNeighborsClassifier(X, Y, verbose=True):
+    if verbose:
+        print("Training KNeighbors Classifier")
+    clf = KNeighborsClassifier(100)
+    clf.fit(X, Y)
+    return clf
+
+def trainGaussianProcessClassifier(X, Y, verbose=True):
+    if verbose:
+        print("Training Gaussian Process Classifier")
+    length_scale = [1 for i in range(len(X[0]))]
+    clf = GaussianProcessClassifier(1.0 * RBF(length_scale), warm_start=True, random_state=42, n_jobs=-1)
+    clf.fit(X, Y)
+    return clf
+
+def trainCustomEnsemble(X, Y, maxDepth=8, estimators=100, verbose=True, subsetTraining=False):
+    if verbose:
+        print("Training custom ensemble")
+    rf = RandomForestClassifier(max_depth=maxDepth, random_state=42)
+    grad = GradientBoostingClassifier(random_state=42)
+    ada = AdaBoostClassifier(n_estimators=estimators, random_state=42)
+    dl = MLPClassifier(hidden_layer_sizes=(256,512,256), random_state=42, max_iter=100)
+    kn = KNeighborsClassifier(100)
+
+    classifiers = [rf, grad, ada, kn]
+
+    Xs = [X for i in range(len(classifiers))]
+    Ys = [Y for i in range(len(classifiers))]
+
+    if subsetTraining:
+        Xs = []
+        Ys = []
+        data = list(zip(X, Y))
+        random.shuffle(data)
+        X_all, Y_all = zip(*data)
+
+        start = 0
+        end = int(len(X)/len(classifiers))
+
+        for i in range(len(classifiers)):
+            Xs.append(X_all[start:end])
+            Ys.append(Y_all[start:end])
+            start = end
+            end = int((i+2) * len(X)/len(classifiers))
+
+    for index in range(len(classifiers)):
+        classifiers[index].fit(Xs[index], Ys[index])
+
+    return CustomEnsembleClassifier(classifiers)
+    
+
+def trainEnsembleClassifier(X, Y, maxDepth=8, estimators=100, verbose=True):
+    if verbose:
+        print("Training an ensemble of Random Forest and Gradient Boosting Classifiers")
+
+    estimators = [
+     ('rf', RandomForestClassifier(max_depth=maxDepth, random_state=42)),
+     ('grad', GradientBoostingClassifier(random_state=42))]
+    clf = StackingClassifier(estimators=estimators, final_estimator=AdaBoostClassifier(n_estimators=50, random_state=42))
+    clf.fit(X, Y)
+    return clf
+
+
+def trainRandomForestClassifier(X, Y, maxDepth=8, verbose=True):
+    if verbose:
+        print("Training Random Forest classifier")
+    clf = RandomForestClassifier(max_depth=maxDepth, random_state=42)
+    clf.fit(X, Y)
+    return clf
+
+def trainAdaBoostClassifier(X, Y, estimators=100, verbose=True):
+    if verbose:
+        print("Training AdaBoosted Decision Tree classifier")
+    clf = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=1), n_estimators=estimators, random_state=42)
+    clf.fit(X, Y)
+    return clf
+
+def trainGradientBoostingClassifier(X, Y, verbose=True):
+    if verbose:
+        print("Training Graident Boosted classifier")
+    clf = GradientBoostingClassifier(random_state=42)
+    clf.fit(X, Y)
+    return clf
+
+def trainSVM(X, Y, verbose=True):
+    if verbose:
+        print("Training SVM classifier")
+    clf = SVC(gamma='auto')
+    clf.fit(X, Y)
+    return clf
---
+++ src/dataUtils.py
@@ -0,0 +1,193 @@
+import numpy as np
+from itertools import zip_longest
+import matplotlib.pyplot as plt
+import random
+from sklearn.metrics import auc
+import statistics
+import os
+
+def compute_exclued_included_sentenceBleuScore(acceptedTranslations, rejectedTranslations):
+    acceptedScore = 0 if len(acceptedTranslations) == 0 else sum([translation.score for translation in acceptedTranslations])/len(acceptedTranslations)
+    
+    rejectedScore = 0 if len(rejectedTranslations) == 0 else sum([translation.score for translation in rejectedTranslations])/len(rejectedTranslations)
+
+    return rejectedScore, acceptedScore
+
+def compute_excluded_included_score (acceptedTranslations, rejectedTranslations):
+    if len(acceptedTranslations) != 0:
+        temporary_reference_inclusion = open("analysis/temporary_reference_inclusion.data", "w")
+        temporary_output_inclusion = open("analysis/temporary_output_inclusion.data", "w")
+
+    
+        for translation in acceptedTranslations:
+            temporary_reference_inclusion.write(translation.reference)
+            temporary_output_inclusion.write(translation.translation)
+
+        temporary_reference_inclusion.close()
+        temporary_output_inclusion.close()
+
+
+        fairseq_command = (f'fairseq-score --sys analysis/temporary_output_inclusion.data'
+                           f'--ref analysis/temporary_reference_inclusion.data'
+                           f' --sacrebleu > analysis/inclusion_result.data')
+        os.system(fairseq_command)
+
+        temporary_inclusion_result = open("analysis/inclusion_result.data")
+        inclusion_result_string = [line for line in temporary_inclusion_result][1].split(" ")[2]
+
+        temporary_reference_inclusion.close()
+        temporary_output_inclusion.close()
+        temporary_inclusion_result.close()
+
+    else:
+        inclusion_result_string = "0"
+
+    if len(rejectedTranslations) != 0:
+
+        temporary_reference_exclusion = open("analysis/temporary_reference_exclusion.data", "w")
+        temporary_output_exclusion = open("analysis/temporary_output_exclusion.data", "w")
+        
+        for translation in rejectedTranslations:
+            temporary_reference_exclusion.write(translation.reference)
+            temporary_output_exclusion.write(translation.translation)
+
+        
+        temporary_reference_exclusion.close()
+        temporary_output_exclusion.close()
+
+        fairseq_command  = (f'fairseq-score --sys analysis/temporary_output_exclusion.data'
+                            f'--ref analysis/temporary_reference_exclusion.data'
+                            f' --sacrebleu > analysis/exclusion_result.data')
+        os.system(fairseq_command)
+
+        temporary_exclusion_result = open("analysis/exclusion_result.data")
+        exclusion_result_string = "0" if len(rejectedTranslations) == 0 else [line for line in temporary_exclusion_result][1].split(" ")[2]
+
+        temporary_reference_exclusion.close()
+        temporary_output_exclusion.close()
+        temporary_exclusion_result.close()
+    
+    else:
+        exclusion_result_string = "0"
+
+    return float(exclusion_result_string), float(inclusion_result_string)
+
+def readTranslations(sentenceFile, featureArray):
+    translations = []
+    temp = []
+    index = 0
+    for line in sentenceFile:
+        if len(temp) < 3:
+            temp.append(line)
+        else:
+            score = float(line.strip("\n"))
+            translations.append(Translation(temp[0], temp[1], temp[2], score, featureArray[index]))
+            index += 1
+            temp = []
+    
+    return translations
+
+def getTrainTestSets(trainTranslations, testTranslations, threshold_train, threshold_test, avgLogProb):
+    trainFeatures = []
+    trainY = []
+    testFeatures = []
+    testY = []
+
+    for translation in trainTranslations:
+        trainFeatures.append(translation.features)
+        if avgLogProb:
+            if translation.features[0] < threshold_train:
+                trainY.append(0)
+            else:
+                trainY.append(1)
+        else:
+            if translation.score < threshold_train:
+                trainY.append(0)
+            else:
+                trainY.append(1)
+    
+    for translation in testTranslations:
+        testFeatures.append(translation.features)
+        if translation.score < threshold_test:
+            testY.append(0)
+        else:
+            testY.append(1)
+
+    return trainFeatures, trainY, testFeatures, testY
+
+
+
+def printDatasetClassProp(Y): 
+    classes = {}
+    total = len(Y)
+    for i in Y:
+        if i in classes:
+            classes[i] += 1
+        else:
+            classes[i] = 1
+    
+    for cls in classes:
+        print("Proportion in class " + str(cls) + " = " + str(classes[cls]/total))
+
+def datasetReader(featureFile, labelFile):
+    files = [featureFile, labelFile]
+
+    X = []
+    Y = []
+
+    for lines in zip_longest(*files, fillvalue=''):
+        currX, currY = lines[0], float(lines[1].strip("\n"))
+        Xarr = []
+        features = currX.split()
+        for feature in features:
+            Xarr.append(float(feature.strip(",").strip("\n")))
+        X.append(Xarr)
+        Y.append(currY)
+    
+    return np.array(X), np.array(Y)
+
+def normalizeFeatures(trainX, testX):
+    featureLists = [trainX[:, i] for i in range(trainX.shape[1])]
+
+    #means and stddv calculated using training features only. 
+    means = [statistics.mean(feature) for feature in featureLists]
+    stdDv = [statistics.stdev(feature) for feature in featureLists]
+
+    trainX = np.array([[(row[i] - means[i]) / stdDv[i] for i in range(len(row))] for row in trainX])
+    testX = np.array([[(row[i] - means[i]) / stdDv[i] for i in range(len(row))] for row in testX])
+
+    return trainX, testX
+
+
+def computeSimilarity(o1, o2):
+    total = len(o1)
+    same = 0
+    for i in range(len(o1)):
+        if o1[i] == o2[i]:
+            same += 1
+    print(same/total)
+
+def calculateAccuracy(predictedClasses, groundTruth):
+    correct_accepted = 0
+    total_accepted = 0
+
+    correct_rejected = 0
+    total_rejected = 0
+
+    for i in range(len(predictedClasses)):
+        if groundTruth[i] == 1:
+            total_accepted += 1
+            if predictedClasses[i] == groundTruth[i]:
+                correct_accepted += 1
+        else:
+            total_rejected += 1
+            if predictedClasses[i] == groundTruth[i]:
+                correct_rejected += 1
+
+
+    print("Correctly accepted = " + str(correct_accepted/total_accepted))
+    print("Incorrectly rejected = " + str(1 - correct_accepted/total_accepted))
+    print("Correctly rejected = " + str(correct_rejected/total_rejected))
+    print("Incorrectly accepted = " + str(1 - correct_rejected/total_rejected))
+
+    print("Total Accuracy = " + str((correct_accepted + correct_rejected)/(total_accepted + total_rejected)))
